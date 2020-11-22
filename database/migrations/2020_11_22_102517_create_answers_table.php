93948173ed3bc177a5e9f4e58d05c6cdcfff5039
Katharina Barth <barth@quellwerke.de>
2020-11-22 13:59:35 +0100

    migrations refreshed

---
+++ database/migrations/2020_11_22_102517_create_answers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArticleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->id();
+            $table->text('answer');
+            $table->bigInteger('question_id');
+            $table->bigInteger('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('article');
+    }
+}
